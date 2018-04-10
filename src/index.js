96ad6fca4898dd770d5189e46b260e38a7edb355
github username <github email address>
2018-04-10 13:47:31 -0400

    a

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
+  let form = document.getElementById('pokemon-seach-form')
+  form.addEventListener('submit', function(event){
+    event.preventDefault()
+
+    let inputText = document.getElementById('pokemon-name')
+    addPokemon(inputText.value)
+    inputText.value = ""
+  })
 
 })
