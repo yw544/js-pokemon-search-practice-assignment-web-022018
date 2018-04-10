5e8238a3cf883aff063b5bfaabb7b0bde2a2323e
Ray Blasko <mray6288@gmail.com>
2018-04-10 13:45:45 -0400

    parse data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+
+	for (p of pokemonData.pokemons){
+		pokemon = new Pokemon(p.name, p.sprites.front, p.sprites.back)
+		console.log(pokemon)
+	}
+
 })
